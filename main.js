719a9be9dedbb2e6cc84e27ed6eb1f058076d675
trickster <andmount@gmail.com>
2019-02-02 13:46:53 +0300

    Add game round function, add UI and event handlers for it

--- main.js
+++ main.js
@@ -1,51 +1,73 @@
+/* eslint-disable no-console */
 function computerPlay() {
   if (Math.floor(Math.random() * 3) === 0) {
     return 'paper';
-  } else if (Math.floor(Math.random() * 3) === 1) {
-    return 'rock';
-  } else {
-    return 'scissors';
   }
+  if (Math.floor(Math.random() * 3) === 1) {
+    return 'rock';
+  }
+  return 'scissors';
 }
 
-let computerSelection = computerPlay();
-let playerSelection = prompt('Choose your weapon!').toLowerCase();
+let winScore = 0;
+let loseScore = 0;
+let roundNum = 0;
+const score = document.getElementById('score');
+const buttons = document.getElementById('buttons');
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
+let playerSelection;
+let computerSelection;
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return 0;
-  } else if (
+  }
+
+  if (
     playerSelection !== 'rock'
     && playerSelection !== 'scissors'
     && playerSelection !== 'paper'
   ) {
     return null;
-  } else if (
+  }
+
+  if (
     (playerSelection === 'rock' && computerSelection === 'scissors')
     || (playerSelection === 'scissors' && computerSelection === 'paper')
     || (playerSelection === 'paper' && computerSelection === 'rock')
   ) {
     return 1;
-  } else {
-    return -1;
   }
+
+  return -1;
 }
 
-function printResult(roundResult) {
-  roundResult = roundResult(playerSelection, computerSelection);
+function game() {
+  computerSelection = computerPlay();
+  const roundResult = playRound(playerSelection, computerSelection);
   switch (roundResult) {
     case 1:
-      console.log(`You win: your ${playerSelection} beats AI's ${computerSelection}`);
+      score.textContent = `Wins:${++winScore} Loses:${loseScore} Round:${++roundNum}`;
       break;
     case -1:
-      console.log(`You LOSE: AI's ${computerSelection} beats your ${playerSelection}`);
+      score.textContent = `Wins:${winScore} Loses:${++loseScore} Round:${++roundNum}`;
       break;
     case 0:
-      console.log(`It's draw with ${playerSelection}`);
+      score.textContent = `Wins:${++winScore} Loses:${++loseScore} Round:${++roundNum}`;
       break;
     default:
-      console.log("Wrong value try to type it again")
   }
 }
 
-printResult(playRound);
+rock.onclick = () => { playerSelection = 'rock'; };
+scissors.onclick = () => { playerSelection = 'scissors'; };
+paper.onclick = () => { playerSelection = 'paper'; };
+buttons.onclick = function () {
+  if (roundNum < 5) {
+    game();
+  } else {
+    score.textContent =  winScore > loseScore ? 'Humanity can be proud. You kick metal ass' : 'Shame on you!';
+  }
+};
