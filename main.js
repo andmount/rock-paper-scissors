d38ff98a1eddca341f59436bcf9d9eababfed9f2
trickster <andmount@gmail.com>
2019-02-06 14:55:56 +0300

    add right output messages, add reset for score board and output paragraph

--- main.js
+++ main.js
@@ -12,11 +12,13 @@ function computerPlay() {
 let winScore = 0;
 let loseScore = 0;
 let roundNum = 0;
+const output = document.getElementById('output');
 const score = document.getElementById('score');
 const buttons = document.getElementById('buttons');
 let rock = document.getElementById('rock');
 let paper = document.getElementById('paper');
 let scissors = document.getElementById('scissors');
+let reset = document.getElementById('reset');
 let playerSelection;
 let computerSelection;
 
@@ -49,12 +51,15 @@ function game() {
   const roundResult = playRound(playerSelection, computerSelection);
   switch (roundResult) {
     case 1:
+      output.textContent = `Your ${playerSelection} beats computer's ${computerSelection}`;
       score.textContent = `Wins:${++winScore} Loses:${loseScore} Round:${++roundNum}`;
       break;
     case -1:
+      output.textContent = `Computer's ${computerSelection} beats your ${playerSelection}`;
       score.textContent = `Wins:${winScore} Loses:${++loseScore} Round:${++roundNum}`;
       break;
     case 0:
+      output.textContent = `Both of you have chosen ${computerSelection}`;
       score.textContent = `Wins:${++winScore} Loses:${++loseScore} Round:${++roundNum}`;
       break;
     default:
@@ -64,10 +69,28 @@ function game() {
 rock.onclick = () => { playerSelection = 'rock'; };
 scissors.onclick = () => { playerSelection = 'scissors'; };
 paper.onclick = () => { playerSelection = 'paper'; };
-buttons.onclick = function () {
+buttons.onclick = () => {
   if (roundNum < 5) {
     game();
   } else {
-    score.textContent =  winScore > loseScore ? 'Humanity can be proud. You kick metal ass' : 'Shame on you!';
+
+    if (winScore > loseScore) {
+      output.textContent = 'Humanity can be proud. You kick metal ass!';
+      output.style.color = 'green';
+    } else if (winScore === loseScore) {
+      output.textContent = "It's draw, but war doesn't end...";
+    } else {
+      output.textContent = "You lose, humanity is dead :^)";
+      output.style.color = 'red';
+    }
   }
 };
+
+reset.onclick = () => { 
+  winScore = 0;
+  loseScore = 0;
+  roundNum = 0;
+  output.textContent = 'Choose your weapon:';
+  output.style.color = 'black';
+  score.textContent = 'Wins:0 Loses:0 Round:0';
+ };
